266b5464454eb91589e8ffbfb628f361b3a471e9
Jorge <up201305617@fe.up.pt>
2017-10-31 10:58:31 +0000

    Printer - working on main screen

--- PrinterApp/app/src/main/java/com/example/jorge/printerapp/Activities/MainScreen.java
+++ PrinterApp/app/src/main/java/com/example/jorge/printerapp/Activities/MainScreen.java
@@ -1,7 +1,9 @@
-package com.example.jorge.printerapp;
+package com.example.jorge.printerapp.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.jorge.printerapp.R;
 
 public class MainScreen extends AppCompatActivity {
 
---
+++ PrinterApp/app/src/main/java/com/example/jorge/printerapp/Utils/Routes.java
@@ -0,0 +1,8 @@
+package com.example.jorge.printerapp.Utils;
+
+/**
+ * Created by Jorge on 31/10/2017.
+ */
+
+public class Routes {
+}
